1511137073b8cc2405c07cd20a175c87fb3ddfc1
hellowordmygosa <claricebanuelosnp68@gmail.com>
2021-03-02 13:52:05 +0800

    3.2

---
+++ 3.2_for/for_3.2.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+#include<Windows.h>
+int main()
+{
+	int i = 0;
+	int j = 0;
+	int f = 0;
+	for (; i < 10; i++)
+	{
+		for (; j < 10; j++)
+		{
+			f++;
+			printf("haha %d  ",f);
+		}
+
+
+
+
+	}
+
+
+
+
+
+
+	system("pause");
+	return 0;
+}
