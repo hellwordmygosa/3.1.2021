54745fd76dca6e31967965e91a5abc199ae3cefc
hellowordmygosa <claricebanuelosnp68@gmail.com>
2021-03-05 23:54:51 +0800

    3.5

--- hanshu_1/hanshu.c
+++ hanshu_1/hanshu.c
@@ -51,45 +51,159 @@
 //	return 0;
 //
 //}
+//调用函数指针比较大小
+//#include<stdio.h>
+//#include<Windows.h>
+//
+//int get_max(int x, int y)
+//{
+//	if (x > y)
+//		return x;
+//	else
+//	{
+//		return y;
+//	}
+//}
+//
+//int get_max2(int x, int y)
+//{
+//	int z = x > y ? x : y;
+//	return z;
+//}
+//
+//void swap(int *z, int *v)
+//{
+//	int tem=0;
+//	tem = *z;
+//	*z = *v;
+//	*v = tem;
+//
+//
+//}
+////用指针交换两个数的大小
+//int main()
+//{
+//	int a = 10;
+//	int b = 5;
+//	//int c = get_max2(a, b);
+//	//printf("%d\n", c);
+//	printf("%d,%d\n", a, b);
+//	swap(&a, &b);
+//	printf("%d,%d\n", a, b);
+//
+//	system("pause");
+//	return 0;
+//}
+//
+//#include<stdio.h>
+//#include<Windows.h>
+//#include<math.h>
+//int re_pime(int n)
+//{
+//	int j;
+//	for (j = 2; j <= sqrt(n); j++)
+//	{
+//		if (n%j == 0)
+//			return 0;
+//	}
+//	return 1;
+//}
+//
+//
+//int main()
+//{
+//	int i = 0;
+//	for (i = 100; i <= 200; i++)
+//	{
+//		//判断一个数是不是素数,不是
+//		if (re_pime(i) == 1)
+//		{
+//			printf("%d\n", i);
+//		}
+//			
+//	}
+//
+//
+//	system("pause");
+//	return 0;
+//
+//}
 
+////是否为润年
+//
+//#include<stdio.h>
+//#include<Windows.h>
+//
+//
+//int is_leap_year(int y)
+//{
+//	if ((y % 4 == 0 && y % 100 != 0) || (y % 400 == 0))
+//	{
+//		return 1;
+//	}
+//
+//}
+//int main()
+//{
+//	int year = 0;
+//	for (year = 1000; year <= 2000; year++)
+//	{
+//		if (is_leap_year(year) == 1)
+//			printf("%d ", year);
+//
+//	}
+//
+//
+//	system("pause");
+//	return 0;
+//}
+
+//二分查找
 #include<stdio.h>
 #include<Windows.h>
 
-int get_max(int x, int y)
+
+int binary_search(int arr[], int k,int sz)
 {
-	if (x > y)
-		return x;
-	else
+	int left = 0;
+	//int sz = sizeof(arr) / sizeof(arr[0]);
+	int right = sz - 1;
+
+	while (left<=right)
 	{
-		return y;
+		int mid = (left + right) / 2;
+		if (mid < k)
+		{
+			left = mid + 1;
+
+		}
+		else if (mid>k)
+		{
+			right = mid - 1;
+		}
+		else
+		{
+			return mid;
+		}
+
 	}
+
+     return -1;
 }
-
-int get_max2(int x, int y)
-{
-	int z = x > y ? x : y;
-	return z;
-}
-
-void swap(int *z, int *v)
-{
-	int tem=0;
-	tem = *z;
-	*z = *v;
-	*v = tem;
-
-
-}
-//用指针交换两个数的大小
 int main()
 {
-	int a = 10;
-	int b = 5;
-	//int c = get_max2(a, b);
-	//printf("%d\n", c);
-	printf("%d,%d\n", a, b);
-	swap(&a, &b);
-	printf("%d,%d\n", a, b);
+	int arr[] = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+	int k = 7;
+	int sz = sizeof(arr) / sizeof(arr[0]);
+	int	bin=binary_search(arr, k,sz);
+	if (bin == -1)
+	{
+		printf("没有找到\n");
+	}
+	else
+	{
+		printf("%d \n", bin);
+	}
 
 	system("pause");
 	return 0;
