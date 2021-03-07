5d94813e2e2cb98fddc36ec80455f784c1bb1a35
hellowordmygosa <claricebanuelosnp68@gmail.com>
2021-03-07 19:33:10 +0800

    3.7

--- digui_1_3.7/digui_3.7.c
+++ digui_1_3.7/digui_3.7.c
@@ -1,13 +1,36 @@
 #include<stdio.h>
 #include<Windows.h>
+#include"Add.h"
+//int main()
+//{
+//
+//	int a = 10;
+//	int b = 20;
+//	int	sum = Add(a, b);
+//	printf("%d\n", sum);
+//
+//
+//
+//
+//	system("pause");
+//	return 0;
+//}
+
+int print(int i)
+{
+	if (i > 9)//1234
+	{
+		print(i / 10);
+	}
+	printf("%d ", i%10);
+
+}
+
 int main()
 {
-
-
-
-
-
-
+	unsigned int i = 0;
+	scanf("%d", &i);
+	print(i);
 	system("pause");
 	return 0;
 }
